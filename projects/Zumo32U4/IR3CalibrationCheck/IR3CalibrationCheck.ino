6268db3a6a9b08d183227203a480e01d24cda877
Olavi Kamppari <OliviliK@users.noreply.github.com>
2017-07-04 23:00:30 -0400

    Add files via upload

---
+++ projects/Zumo32U4/IR3CalibrationCheck/IR3CalibrationCheck.ino
@@ -0,0 +1,28 @@
+/**
+ * Created by Olavi Kamppari on 7/4/2017.
+ */
+
+/**
+ * Used to test the line sensor calibration values stored in ZumoIR3.cpp
+ */
+ 
+#include <Zumo32U4.h>
+#include <ZumoIR3.h>
+
+Zumo32U4LCD         lcd;
+ZumoIR3             IR3;
+
+void setup() {
+}
+
+void loop() {
+  lcd.clear();
+  lcd.gotoXY(3, 0);
+  lcd.print(IR3.readFront2());
+  lcd.gotoXY(0, 1);
+  lcd.print(IR3.readLeft2());
+  lcd.gotoXY(5, 1);
+  lcd.print(IR3.readRight2());
+  delay(20);
+}
+
